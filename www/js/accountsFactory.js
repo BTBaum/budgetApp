c3621012d1e18a1c3e74dfcbe79a6a5adb2cd24f
Brandon <railsdev14@gmail.com>
2015-12-02 13:50:55 -0500

    Add account list to account.html

--- www/js/accountsFactory.js
+++ www/js/accountsFactory.js
@@ -1,5 +1,6 @@
 angular.module('budgetApp')
   .factory("Accounts", function($firebaseArray) {
     var accountsRef = new Firebase("https://bigpicture.firebaseio.com/accounts");
+    console.log(accountsRef);
     return $firebaseArray(accountsRef);
   })
